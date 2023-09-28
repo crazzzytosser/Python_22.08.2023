edb03697cc9b29b70e96fbeb1afdeec7e055c5ff
Your Name <you@example.com>
2023-09-28 11:13:35 +0300

    Itog

--- HomeWorks/Itog/Itog.py
+++ HomeWorks/Itog/Itog.py
@@ -5,7 +5,7 @@ numbers = [random.choice([0, 1]) for _ in range(10)]  # Создаем спис�
 human = numbers.copy()
 roboT = [1 - num for num in numbers]
 
-print(f"{' '}{'Human':^15}{'RoboT':^10}")
+print(f"{'№'}{'Human':^15}{'RoboT':^10}")
 print('*' * 25)
 
 for i in range(len(human)):
