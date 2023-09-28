a804e3fd76bfbdabb2384c6d625f5aee035abde8
Your Name <you@example.com>
2023-09-28 11:11:47 +0300

    Itog

---
+++ HomeWorks/Itog/Itog.py
@@ -0,0 +1,12 @@
+import random
+
+numbers = [random.choice([0, 1]) for _ in range(10)]  # Создаем список случайных чисел от 0 до 1
+
+human = numbers.copy()
+roboT = [1 - num for num in numbers]
+
+print(f"{' '}{'Human':^15}{'RoboT':^10}")
+print('*' * 25)
+
+for i in range(len(human)):
+    print(f'{i + 1}{human[i]:^15}{roboT[i]:^10}')
